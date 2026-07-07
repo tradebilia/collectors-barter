2226d3425103bb43330f4bffa4b4addae53a7b6d
tradebilia <2.88351868e+08+tradebilia@users.noreply.github.com>
2026-07-07 01:06:05 +0000

    UI (2026-07-07): Further increase Collector's Forum hero logo size

--- client/src/pages/Forum.tsx
+++ client/src/pages/Forum.tsx
@@ -41,8 +41,8 @@ export function Forum() {
           backgroundPosition: 'center',
           backgroundRepeat: 'no-repeat'
         }} />
-        <div className="container relative flex h-64 items-center justify-center py-0 sm:h-72 sm:py-0 lg:h-80 lg:py-0">
-          <div className="flex w-full max-w-5xl items-center justify-center">
+        <div className="container relative flex h-72 items-center justify-center py-0 sm:h-80 sm:py-0 lg:h-96 lg:py-0">
+          <div className="flex w-full max-w-6xl items-center justify-center">
             <img
               src="/images/forum-title.svg"
               alt="Collector's Forum"
